ab10f782476e778f9fa4eb4b64ca4ff02a5f30c2
fayyozbobokulov <b.fayyoz2000@gmail.com>
2022-10-15 02:27:39 +0500

    BC-3: Account - find all return type is added.

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { AccountService } from './account.service';
+import { IAccount } from './types/account.interface';
 
 @Controller('account')
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
 
   @Get()
-  findAll() {
+  findAll(): IAccount[] {
     return this.accountService.findAll();
   }
 
